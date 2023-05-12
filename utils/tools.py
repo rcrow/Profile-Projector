c4d3adb38cb030dab4fb1b0fda13256d4cd1651b
Daniel Miranda <dmiranda@usgs.gov>
2023-05-12 16:57:01 -0500

    Add conditional to execution to only do near/join/extract without split if there is no zone. Use a function for near/join/extract. add step to copy the input projection layer, since all GP Tools change the input.

--- utils/tools.py
+++ utils/tools.py
@@ -127,82 +127,96 @@ class Projector(object):
         OutputFeatureClass = parameters[5].valueAsText
         dem = parameters[6].valueAsText
 
-        miles_spatial = arcpy.Describe(parameters[1].value).spatialReference
-        to_project_spatial = arcpy.Describe(parameters[0].value).spatialReference
+        def near_join(projected, river, distance_field, dem):
+            near = arcpy.analysis.Near(in_features=projected,
+                                       near_features=river, search_radius="#",
+                                       location="LOCATION", angle="ANGLE", method="PLANAR")
 
-        arcpy.management.CreateFeatureDataset(arcpy.env.scratchWorkspace, "MilesSplit",
-                                              miles_spatial)
-        arcpy.management.CreateFeatureDataset(arcpy.env.scratchWorkspace, "PointsSplit",
-                                              to_project_spatial)
+            join = arcpy.management.JoinField(in_data=near, in_field="NEAR_FID",
+                                       join_table=river,
+                                       join_field="OBJECTID", fields=distance_field)
 
-        scratch = arcpy.env.scratchWorkspace
+            arcpy.gp.ExtractMultiValuesToPoints_sa(join, dem, "NONE")
 
-        # Split Points of Interest into zones
-        messages.addMessage("Splitting points based on zones ...")
-        arcpy.analysis.Split(in_features=toProjectFile, split_features=zonesFile, split_field=zone_field,
-                             out_workspace=scratch + "/PointsSplit", cluster_tolerance="#")
+        if zonesFile is None:
+            output = arcpy.management.Copy(toProjectFile, OutputFeatureClass)
+            near_join(output, milesFile, distance_field, dem)
 
-        # Rename feature classes
-        arcpy.env.workspace = scratch
-        pointsList = arcpy.ListFeatureClasses('', '', "PointsSplit")
-        # messages.addMessage(pointsList)
-        for stuff in pointsList:
-            arcpy.management.Rename(str(stuff), str(stuff) + "_pt")
+        if zonesFile is not None:
+            miles_spatial = arcpy.Describe(parameters[1].value).spatialReference
+            to_project_spatial = arcpy.Describe(parameters[0].value).spatialReference
 
-        # Split Miles into zones
-        messages.addMessage("Splitting miles based on zones ...")
-        arcpy.analysis.Split(in_features=milesFile, split_features=zonesFile, split_field=zone_field,
-                             out_workspace=scratch + "/MilesSplit", cluster_tolerance="#")
+            arcpy.management.CreateFeatureDataset(arcpy.env.scratchWorkspace, "MilesSplit",
+                                                  miles_spatial)
+            arcpy.management.CreateFeatureDataset(arcpy.env.scratchWorkspace, "PointsSplit",
+                                                  to_project_spatial)
 
-        # Rename feature classes
-        arcpy.env.workspace = scratch
-        milesList = arcpy.ListFeatureClasses('', '', "MilesSplit")
-        messages.addMessage(milesList)
-        for junk in milesList:
-            arcpy.management.Rename(str(junk), str(junk) + "_mi")
+            scratch = arcpy.env.scratchWorkspace
 
-        # Find zones with points for projection
-        zonesList = arcpy.ListFeatureClasses('', '', "PointsSplit")
-        messages.addMessage(zonesList)
+            # Split Points of Interest into zones
+            messages.addMessage("Splitting points based on zones ...")
+            arcpy.analysis.Split(in_features=toProjectFile, split_features=zonesFile, split_field=zone_field,
+                                 out_workspace=scratch + "/PointsSplit", cluster_tolerance="#")
 
-        # Find zones with points for projection
-        for item in zonesList:
-            if arcpy.Exists(scratch + "/PointsSplit/" + item) and arcpy.Exists(
-                    scratch + "/MilesSplit/" + item[0:-3] + "_mi"):
-                messages.addMessage("Working on zone: " + str(item))
-                messages.addMessage("Finding closest points ...")
-                arcpy.analysis.Near(in_features=scratch + "/PointsSplit/" + item,
-                                    near_features=scratch + "/MilesSplit/" + item[0:-3] + "_mi", search_radius="#",
-                                    location="LOCATION", angle="ANGLE", method="PLANAR")
-                messages.addMessage("Joining ...")
-                arcpy.management.JoinField(in_data=scratch + "/PointsSplit/" + item, in_field="NEAR_FID",
-                                           join_table=scratch + "/MilesSplit/" + item[0:-3] + "_mi",
-                                           join_field="OBJECTID", fields=distance_field)
-                messages.addMessage("Extracting elevation...")
-                arcpy.gp.ExtractMultiValuesToPoints_sa(scratch + "/PointsSplit/" + item, dem, "NONE")
+            # Rename feature classes
+            arcpy.env.workspace = scratch
+            pointsList = arcpy.ListFeatureClasses('', '', "PointsSplit")
+            # messages.addMessage(pointsList)
+            for stuff in pointsList:
+                arcpy.management.Rename(str(stuff), str(stuff) + "_pt")
 
-        # Creat list with full path to output feature classes for merging
-        listLength = len(zonesList)
-        # messages.addMessage(str(zonesList))
-        # messages.addMessage("listLength: "+str(listLength))
-        listPaths = [scratch + "\\PointsSplit\\"] * listLength
-        # messages.addMessage(str(listPaths))
-        mergedList = []
+            # Split Miles into zones
+            messages.addMessage("Splitting miles based on zones ...")
+            arcpy.analysis.Split(in_features=milesFile, split_features=zonesFile, split_field=zone_field,
+                                 out_workspace=scratch + "/MilesSplit", cluster_tolerance="#")
 
-        for x in range(0, listLength):
-            # messages.addMessage("x="+str(x))
-            mergedList.append(listPaths[x] + zonesList[x])
+            # Rename feature classes
+            arcpy.env.workspace = scratch
+            milesList = arcpy.ListFeatureClasses('', '', "MilesSplit")
+            messages.addMessage(milesList)
+            for junk in milesList:
+                arcpy.management.Rename(str(junk), str(junk) + "_mi")
 
-        inputsString = ';'.join(mergedList)
-        messages.addMessage(inputsString)
-        messages.addMessage(OutputFeatureClass)
+            # Find zones with points for projection
+            zonesList = arcpy.ListFeatureClasses('', '', "PointsSplit")
+            messages.addMessage(zonesList)
 
-        # arcpy.management.Merge(inputs="E:/users/rcrow/Documents/ArcGIS/Default.gdb/PointsSplit/Blythe_pt;E:/users/rcrow/Documents/ArcGIS/Default.gdb/PointsSplit/Laughlin_pt",output="E:/users/rcrow/Documents/ArcGIS/Default.gdb/Blythe_pt_Merge",field_mappings="""SampNum "SampNum" true true false 255 Text 0 0 ,First,#,E:/users/rcrow/Documents/ArcGIS/Default.gdb/PointsSplit/Blythe_pt,SampNum,-1,-1,E:/users/rcrow/Documents/ArcGIS/Default.gdb/PointsSplit/Laughlin_pt,SampNum,-1,-1;Area "Area" true true false 255 Text 0 0 ,First,#,E:/users/rcrow/Documents/ArcGIS/Default.gdb/PointsSplit/Blythe_pt,Area,-1,-1,E:/users/rcrow/Documents/ArcGIS/Default.gdb/PointsSplit/Laughlin_pt,Area,-1,-1;Person "Person" true true false 255 Text 0 0 ,First,#,E:/users/rcrow/Documents/ArcGIS/Default.gdb/PointsSplit/Blythe_pt,Person,-1,-1,E:/users/rcrow/Documents/ArcGIS/Default.gdb/PointsSplit/Laughlin_pt,Person,-1,-1;Material "Material" true true false 255 Text 0 0 ,First,#,E:/users/rcrow/Documents/ArcGIS/Default.gdb/PointsSplit/Blythe_pt,Material,-1,-1,E:/users/rcrow/Documents/ArcGIS/Default.gdb/PointsSplit/Laughlin_pt,Material,-1,-1;StatUnit "StatUnit" true true false 255 Text 0 0 ,First,#,E:/users/rcrow/Documents/ArcGIS/Default.gdb/PointsSplit/Blythe_pt,StatUnit,-1,-1,E:/users/rcrow/Documents/ArcGIS/Default.gdb/PointsSplit/Laughlin_pt,StatUnit,-1,-1;Notes "Notes" true true false 255 Text 0 0 ,First,#,E:/users/rcrow/Documents/ArcGIS/Default.gdb/PointsSplit/Blythe_pt,Notes,-1,-1,E:/users/rcrow/Documents/ArcGIS/Default.gdb/PointsSplit/Laughlin_pt,Notes,-1,-1;Date "Date" true true false 255 Text 0 0 ,First,#,E:/users/rcrow/Documents/ArcGIS/Default.gdb/PointsSplit/Blythe_pt,Date,-1,-1,E:/users/rcrow/Documents/ArcGIS/Default.gdb/PointsSplit/Laughlin_pt,Date,-1,-1;OrigX "OrigX" true true false 8 Double 0 0 ,First,#,E:/users/rcrow/Documents/ArcGIS/Default.gdb/PointsSplit/Blythe_pt,OrigX,-1,-1,E:/users/rcrow/Documents/ArcGIS/Default.gdb/PointsSplit/Laughlin_pt,OrigX,-1,-1;OrigY "OrigY" true true false 8 Double 0 0 ,First,#,E:/users/rcrow/Documents/ArcGIS/Default.gdb/PointsSplit/Blythe_pt,OrigY,-1,-1,E:/users/rcrow/Documents/ArcGIS/Default.gdb/PointsSplit/Laughlin_pt,OrigY,-1,-1;XYerror "XYerror" true true false 8 Double 0 0 ,First,#,E:/users/rcrow/Documents/ArcGIS/Default.gdb/PointsSplit/Blythe_pt,XYerror,-1,-1,E:/users/rcrow/Documents/ArcGIS/Default.gdb/PointsSplit/Laughlin_pt,XYerror,-1,-1;ErrorUnit "ErrorUnit" true true false 255 Text 0 0 ,First,#,E:/users/rcrow/Documents/ArcGIS/Default.gdb/PointsSplit/Blythe_pt,ErrorUnit,-1,-1,E:/users/rcrow/Documents/ArcGIS/Default.gdb/PointsSplit/Laughlin_pt,ErrorUnit,-1,-1;OrigDat "OrigDat" true true false 255 Text 0 0 ,First,#,E:/users/rcrow/Documents/ArcGIS/Default.gdb/PointsSplit/Blythe_pt,OrigDat,-1,-1,E:/users/rcrow/Documents/ArcGIS/Default.gdb/PointsSplit/Laughlin_pt,OrigDat,-1,-1;OrigCS "OrigCS" true true false 255 Text 0 0 ,First,#,E:/users/rcrow/Documents/ArcGIS/Default.gdb/PointsSplit/Blythe_pt,OrigCS,-1,-1,E:/users/rcrow/Documents/ArcGIS/Default.gdb/PointsSplit/Laughlin_pt,OrigCS,-1,-1;XYmeth "XYmeth" true true false 255 Text 0 0 ,First,#,E:/users/rcrow/Documents/ArcGIS/Default.gdb/PointsSplit/Blythe_pt,XYmeth,-1,-1,E:/users/rcrow/Documents/ArcGIS/Default.gdb/PointsSplit/Laughlin_pt,XYmeth,-1,-1;Long "Long" true true false 8 Double 0 0 ,First,#,E:/users/rcrow/Documents/ArcGIS/Default.gdb/PointsSplit/Blythe_pt,Long,-1,-1,E:/users/rcrow/Documents/ArcGIS/Default.gdb/PointsSplit/Laughlin_pt,Long,-1,-1;Lat "Lat" true true false 8 Double 0 0 ,First,#,E:/users/rcrow/Documents/ArcGIS/Default.gdb/PointsSplit/Blythe_pt,Lat,-1,-1,E:/users/rcrow/Documents/ArcGIS/Default.gdb/PointsSplit/Laughlin_pt,Lat,-1,-1;LocConv "LocConv" true true false 255 Text 0 0 ,First,#,E:/users/rcrow/Documents/ArcGIS/Default.gdb/PointsSplit/Blythe_pt,LocConv,-1,-1,E:/users/rcrow/Documents/ArcGIS/Default.gdb/PointsSplit/Laughlin_pt,LocConv,-1,-1;OrigElev "OrigElev" true true false 255 Text 0 0 ,First,#,E:/users/rcrow/Documents/ArcGIS/Default.gdb/PointsSplit/Blythe_pt,OrigElev,-1,-1,E:/users/rcrow/Documents/ArcGIS/Default.gdb/PointsSplit/Laughlin_pt,OrigElev,-1,-1;ElevMeth "ElevMeth" true true false 255 Text 0 0 ,First,#,E:/users/rcrow/Documents/ArcGIS/Default.gdb/PointsSplit/Blythe_pt,ElevMeth,-1,-1,E:/users/rcrow/Documents/ArcGIS/Default.gdb/PointsSplit/Laughlin_pt,ElevMeth,-1,-1;Elev "Elev" true true false 8 Double 0 0 ,First,#,E:/users/rcrow/Documents/ArcGIS/Default.gdb/PointsSplit/Blythe_pt,Elev,-1,-1,E:/users/rcrow/Documents/ArcGIS/Default.gdb/PointsSplit/Laughlin_pt,Elev,-1,-1;ElevSource "ElevSource" true true false 255 Text 0 0 ,First,#,E:/users/rcrow/Documents/ArcGIS/Default.gdb/PointsSplit/Blythe_pt,ElevSource,-1,-1,E:/users/rcrow/Documents/ArcGIS/Default.gdb/PointsSplit/Laughlin_pt,ElevSource,-1,-1;NEAR_FID "NEAR_FID" true true false 4 Long 0 0 ,First,#,E:/users/rcrow/Documents/ArcGIS/Default.gdb/PointsSplit/Blythe_pt,NEAR_FID,-1,-1,E:/users/rcrow/Documents/ArcGIS/Default.gdb/PointsSplit/Laughlin_pt,NEAR_FID,-1,-1;NEAR_DIST "NEAR_DIST" true true false 8 Double 0 0 ,First,#,E:/users/rcrow/Documents/ArcGIS/Default.gdb/PointsSplit/Blythe_pt,NEAR_DIST,-1,-1,E:/users/rcrow/Documents/ArcGIS/Default.gdb/PointsSplit/Laughlin_pt,NEAR_DIST,-1,-1;NEAR_X "NEAR_X" true true false 8 Double 0 0 ,First,#,E:/users/rcrow/Documents/ArcGIS/Default.gdb/PointsSplit/Blythe_pt,NEAR_X,-1,-1,E:/users/rcrow/Documents/ArcGIS/Default.gdb/PointsSplit/Laughlin_pt,NEAR_X,-1,-1;NEAR_Y "NEAR_Y" true true false 8 Double 0 0 ,First,#,E:/users/rcrow/Documents/ArcGIS/Default.gdb/PointsSplit/Blythe_pt,NEAR_Y,-1,-1,E:/users/rcrow/Documents/ArcGIS/Default.gdb/PointsSplit/Laughlin_pt,NEAR_Y,-1,-1;NEAR_ANGLE "NEAR_ANGLE" true true false 8 Double 0 0 ,First,#,E:/users/rcrow/Documents/ArcGIS/Default.gdb/PointsSplit/Blythe_pt,NEAR_ANGLE,-1,-1,E:/users/rcrow/Documents/ArcGIS/Default.gdb/PointsSplit/Laughlin_pt,NEAR_ANGLE,-1,-1;RM_final "RM_final" true true false 2 Short 0 0 ,First,#,E:/users/rcrow/Documents/ArcGIS/Default.gdb/PointsSplit/Blythe_pt,RM_final,-1,-1,E:/users/rcrow/Documents/ArcGIS/Default.gdb/PointsSplit/Laughlin_pt,RM_final,-1,-1;TERRAIN_DEM10Meter_LOCO "TERRAIN_DEM10Meter_LOCO" true true false 4 Float 0 0 ,First,#,E:/users/rcrow/Documents/ArcGIS/Default.gdb/PointsSplit/Blythe_pt,TERRAIN_DEM10Meter_LOCO,-1,-1,E:/users/rcrow/Documents/ArcGIS/Default.gdb/PointsSplit/Laughlin_pt,TERRAIN_DEM10Meter_LOCO,-1,-1""")
+            # Find zones with points for projection
+            for item in zonesList:
+                if arcpy.Exists(scratch + "/PointsSplit/" + item) and arcpy.Exists(
+                        scratch + "/MilesSplit/" + item[0:-3] + "_mi"):
+                    messages.addMessage("Working on zone: " + str(item))
+                    messages.addMessage("Finding closest points ...")
+                    arcpy.analysis.Near(in_features=scratch + "/PointsSplit/" + item,
+                                        near_features=scratch + "/MilesSplit/" + item[0:-3] + "_mi", search_radius="#",
+                                        location="LOCATION", angle="ANGLE", method="PLANAR")
+                    messages.addMessage("Joining ...")
+                    arcpy.management.JoinField(in_data=scratch + "/PointsSplit/" + item, in_field="NEAR_FID",
+                                               join_table=scratch + "/MilesSplit/" + item[0:-3] + "_mi",
+                                               join_field="OBJECTID", fields=distance_field)
+                    messages.addMessage("Extracting elevation...")
+                    arcpy.gp.ExtractMultiValuesToPoints_sa(scratch + "/PointsSplit/" + item, dem, "NONE")
 
-        arcpy.management.Merge(inputs=inputsString, output=OutputFeatureClass)
+            # Creat list with full path to output feature classes for merging
+            listLength = len(zonesList)
+            # messages.addMessage(str(zonesList))
+            # messages.addMessage("listLength: "+str(listLength))
+            listPaths = [scratch + "\\PointsSplit\\"] * listLength
+            # messages.addMessage(str(listPaths))
+            mergedList = []
 
-        arcpy.management.Delete(in_data=scratch + "/MilesSplit", data_type="FeatureDataset")
-        arcpy.management.Delete(in_data=scratch + "/PointsSplit", data_type="FeatureDataset")
+            for x in range(0, listLength):
+                # messages.addMessage("x="+str(x))
+                mergedList.append(listPaths[x] + zonesList[x])
+
+            inputsString = ';'.join(mergedList)
+            messages.addMessage(inputsString)
+            messages.addMessage(OutputFeatureClass)
+
+            arcpy.management.Merge(inputs=inputsString, output=OutputFeatureClass)
+
+            arcpy.management.Delete(in_data=scratch + "/MilesSplit", data_type="FeatureDataset")
+            arcpy.management.Delete(in_data=scratch + "/PointsSplit", data_type="FeatureDataset")
 
         removeNulls = parameters[7].valueAsText
         if removeNulls == "true":
